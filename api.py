fc93e177ded5fe4e749a50594c2b1f26a6baa71d
jamesedra <delstherjamesedralin@gmail.com>
2023-03-25 14:12:51 -0700

    Combined api and fetch

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 import sqlite3
 import random
+from flask import Flask, render_template, request
 
 # Connect to the database
 conn = sqlite3.connect('emo_track_responses.db')
@@ -60,4 +61,19 @@ def getResponses(UserInput):
         print("Sorry, I don't have a response for that.")
 
 
+## Create a new instance of the Flask class
+app = Flask(__name__)
 
+## Define a route for submitting the form data
+@app.route('/')
+def form():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    user_input = request.form['user_input']
+    response = api.getResponses(user_input)
+    return render_template('response.html', display=response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
